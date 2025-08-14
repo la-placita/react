4c4489993df2d8df3549bd8f8eac23313983d73d
root <jfcodiaz@gmail.com>
2025-08-14 15:07:23 +0000

    [25] 🧪 Agrega y prueba TodoItem.test.jsx y otros cambios recientes en tests y hooks ✨

--- code/05-hook-app/tests/08-useReducer/todoItem.test.jsx
+++ code/05-hook-app/tests/08-useReducer/todoItem.test.jsx
@@ -1,8 +1,32 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { TodoItem } from "../../src/08-useReducer/TodoItem";
 
-describe('debe de mostrar el Todo Pendiente de completar', () => {
-    expect(true).toBe(true);
-    render(<TodoItem/>);
-    screen.debug();
+describe('<TodoItem />', () => {
+
+    let onRemoveItem, onToggleItem;
+
+    beforeEach(() => {
+        onRemoveItem = jest.fn();
+        onToggleItem = jest.fn();
+    });
+
+    test('debe de mostrar el Todo Pendiente de completar', () => {
+        expect(true).toBe(true);
+        const todo = {
+            id: 1,
+            description: 'Demo Todo',
+            done: false
+        };
+        render(<TodoItem todo={todo} onRemoveItem={onRemoveItem} onToggleItem={onToggleItem} />);
+        screen.getByText(todo.description);
+        const span = screen.getByText(todo.description);
+        expect(span.className).toContain('align-self-center');
+        expect(span.className).not.toContain('text-decoration-line-through');
+        const button = screen.getByRole('button');
+        expect(button.className).toContain('btn-danger');
+        fireEvent.click(button);
+        fireEvent.click(span);
+        expect(onToggleItem).toHaveBeenCalledWith(todo.id);
+        expect(onRemoveItem).toHaveBeenCalledWith(todo.id);
+    });
 });
